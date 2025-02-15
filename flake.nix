86f47fcb38d2a0db68254ddee49d956fd29a4720
Alexander Kjäll <alexander.kjall@hafslund.no>
2025-02-15 15:03:23 +0100

    add rust-toolchain

--- flake.nix
+++ flake.nix
@@ -12,44 +12,50 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
 
   let
-    configuration = {pkgs, ... }: {
-
-        #services.nix-daemon.enable = true;
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
-
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        # Used for backwards compatibility. please read the changelog
-        # before changing: `darwin-rebuild changelog`.
-        system.stateVersion = 4;
-
-        # The platform the configuration will be used on.
-        # If you're on an Intel system, replace with "x86_64-darwin"
-        nixpkgs.hostPlatform = "aarch64-darwin";
-
-        # group id of the nix stuff
-        ids.gids.nixbld = 350;
-
-        # Declare the user that will be running `nix-darwin`.
-        users.users."alexander.kjall" = {
-            name = "alexander.kjall";
-            home = "/Users/alexander.kjall";
-        };
-
-        # Create /etc/zshrc that loads the nix-darwin environment.
-        programs.zsh.enable = true;
-
-        environment.systemPackages = [ pkgs.neofetch pkgs.emacs pkgs.git pkgs.cargo pkgs.sequoia-sq pkgs.sequoia-chameleon-gnupg ];
-
-        # enable touch-id for sudo
-        security.pam.enableSudoTouchIdAuth = true;
+    pkgs = import <nixpkgs> {};
+    rust-toolchain = pkgs.symlinkJoin {
+      name = "rust-toolchain";
+      paths = [pkgs.rustc pkgs.cargo pkgs.rustPlatform.rustcSrc];
     };
   in
-  {
+  let
+    configuration = {pkgs, ... }: {
+
+      #services.nix-daemon.enable = true;
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility. please read the changelog
+      # before changing: `darwin-rebuild changelog`.
+      system.stateVersion = 4;
+
+      # The platform the configuration will be used on.
+      # If you're on an Intel system, replace with "x86_64-darwin"
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      # group id of the nix stuff
+      ids.gids.nixbld = 350;
+
+      # Declare the user that will be running `nix-darwin`.
+      users.users."alexander.kjall" = {
+        name = "alexander.kjall";
+        home = "/Users/alexander.kjall";
+      };
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;
+
+      environment.systemPackages = [ pkgs.neofetch pkgs.emacs pkgs.git pkgs.cargo pkgs.sequoia-sq pkgs.sequoia-chameleon-gnupg rust-toolchain];
+
+      # enable touch-id for sudo
+      security.pam.enableSudoTouchIdAuth = true;
+    };
+  in {
     darwinConfigurations."Alexander-sin-MacBook-Pro" = nix-darwin.lib.darwinSystem {
       modules = [
-         configuration
+        configuration
       ];
     };
   };
